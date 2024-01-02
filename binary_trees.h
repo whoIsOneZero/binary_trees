dc1404c9e662d3dbde381c4f254cb2a1cf0143e9
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-01-02 18:05:55 +0000

    add: datastructures

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,9 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
+
+/*->>> DATA STRUCTURES <<<-*/
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,14 +13,28 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
+typedef struct binary_tree_s binary_tree_t;
+
+/* BINARY SEARCH TREE */
+typedef struct binary_tree_s bst_t;
+
+/* AVL TREE */
+typedef struct binary_tree_s avl_t;
+
+/* MAX BINARY HEAP */
+typedef struct binary_tree_s heap_t;
+
+/*---------------------------------------------------*/
+
+/*->>> FUNCTION PROTOTYPES <<<-*/
 void binary_tree_print(const binary_tree_t *);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
