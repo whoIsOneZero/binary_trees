5a14eb3259f9ea3161da76802b9a995bcd0db9b7
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-01-02 21:35:50 +0000

    add: task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *right_child;
+
+    if (parent == NULL )
+        return (NULL);
+
+    /*Create the new node called right_child*/
+    right_child = binary_tree_node(parent, value);
+    /*Node creation failed*/
+    if (right_child == NULL)
+        return (NULL);
+
+    /*`parent` already has a right_child*/
+    if (parent->right != NULL)
+    {
+        /*Set right_child's right-child to be parent's right-child*/
+        right_child->right = parent->right;
+
+        /*Set parent->right to right_child*/
+        parent->right = right_child;
+    }
+    else
+        parent->right = right_child;
+
+    return (right_child);
+}
