a7ee57ed68b0cf6002a23bfe9471d1fc3ad88279
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-01-03 22:58:17 +0000

    fix: Betty compliant

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * void binary_tree_inorder - goes through a bin tree using pre-order traversal.
+ * binary_tree_inorder - goes through a bin tree using pre-order traversal.
  *
  * @tree: a ptr to the root node of the tree to traverse
  * @func: ptr to function to call for each node. the value in the node
@@ -9,13 +9,13 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 
-    binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 
-    /*Apply function to current node.*/
+	/*Apply function to current node.*/
 	func(tree->n);
-    
+
 	binary_tree_inorder(tree->right, func);
 }
