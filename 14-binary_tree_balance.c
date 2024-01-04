0191cf4e7647bc7ad00403af1d9b3c7a0a7413a5
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-01-04 13:09:38 +0000

    task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree.
+ *
+ * @tree: ptr to the root node of the tree to measure b. factor
+ * Return: the balance factor, or 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+}
