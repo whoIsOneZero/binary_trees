6d119ddf66f094bb15e49f59d046e78943c7c639
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-01-05 01:06:28 +0000

    fix: Betty compliant, add tests

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,33 +7,34 @@
  * @second: ptr to the second node.
  * Return: ptr to the lowest common ancestor or NULL if none found
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-    binary_tree_t *parent1, *parent2;
+	binary_tree_t *parent1, *parent2;
 
-    if (first == NULL || second == NULL)
-        return (NULL);
+	if (first == NULL || second == NULL)
+		return (NULL);
 
-    parent1 = first->parent;
-    parent2 = second->parent;
+	parent1 = first->parent;
+	parent2 = second->parent;
 
-    /*Check if 'first' is a child of 'second'*/
-    /*Return 'first' or 'second' based on which is the parent*/
-    if ((first->left == second) || (first->right == second))
-        return ((binary_tree_t *)first);
+	/*Check if 'first' is a child of 'second'*/
+	/*Return 'first' or 'second' based on which is the parent*/
+	if ((first->left == second) || (first->right == second))
+		return ((binary_tree_t *)first);
 
-    if ((second->left == first) || (second->right == first))
-        return ((binary_tree_t *)second);
+	if ((second->left == first) || (second->right == first))
+		return ((binary_tree_t *)second);
 
-    /*Is the parent of 'first' = the parent of 'second'?*/
-    if (parent1 == parent2)
-        return (parent1);
-    /*Recursively call the fuction on 'first' and parent or 'second'*/
-    return (binary_trees_ancestor(first, parent2));
+	/*Is the parent of 'first' = the parent of 'second'?*/
+	if (parent1 == parent2)
+		return (parent1);
+	/*Recursively call the fuction on 'first' and parent or 'second'*/
+	return (binary_trees_ancestor(first, parent2));
 
-    /*Is the parent of 'second' = the parent of 'first'?*/
-    if (parent2 == parent1)
-        return (parent2);
-    /*Recursively call the function on 'second' and parent of 'fisrt'*/
-    return (binary_trees_ancestor(second, parent1));
+	/*Is the parent of 'second' = the parent of 'first'?*/
+	if (parent2 == parent1)
+		return (parent2);
+	/*Recursively call the function on 'second' and parent of 'fisrt'*/
+	return (binary_trees_ancestor(second, parent1));
 }
